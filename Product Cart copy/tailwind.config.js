3bd594b266bed27c767280643ea4b16264eb02ab
Akshesh Sabhadiya <aksheshsabhadiya2021@gmail.com>
2025-03-05 15:36:38 +0530

    apply some css, tailwind css

---
+++ Product Cart copy/tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // Update this based on your file structure
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
