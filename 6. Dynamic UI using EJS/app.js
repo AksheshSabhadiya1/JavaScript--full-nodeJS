7afe92d469abac1af23a8b2f5771f22c5ee907f5
Akshesh Sabhadiya <aksheshsabhadiya2021@gmail.com>
2025-03-06 12:16:14 +0530

    Dynamic UI using EJS

--- 6. Dynamic UI using EJS/app.js
+++ 6. Dynamic UI using EJS/app.js
@@ -2,11 +2,15 @@
 const express = require('express')
 const app = express()
 const port = 3002
-const userRouter = require('./Routes/userRouter')
-const hostRouter = require('./Routes/hostRouter')
+const userRouter = require('./routes/userRouter')
+const {hostRouter} = require('./routes/hostRouter')
 const path = require('path')
 const rootDir = require('./Utils/PathUtil')
 
+
+// EJS setup
+app.set('view engine','ejs')
+app.set('views','views')
 
 app.use((req, res, next)=>{
   console.log(req.url, req.method);
@@ -17,12 +21,11 @@ app.use(express.urlencoded())
 app.use(userRouter)
 app.use('/host',hostRouter)
 
-app.use(express.static(path.join(rootDir, 'Public')))
+app.use(express.static(path.join(rootDir, 'public')))
 
 app.use((req, res, next)=>{
-  // res.status(404).send('<h1>404 Page Not Found</h1>')
-  // res.status(404).sendFile(path.join(__dirname, 'Views', '404.html'))
-    res.status(404).sendFile(path.join(rootDir, 'Views', '404.html'))
+    // res.status(404).sendFile(path.join(rootDir, 'views', '404.html'))
+    res.status(404).render('404',{pageTitle: '404 Page Not Found'})
 })
 
 app.listen(port, ()=>{
